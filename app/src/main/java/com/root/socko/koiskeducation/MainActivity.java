d9ba3c1bd3105c9297010ed5b3d166ea8e0d5719
ashishghadi27 <ashishghadi27@gmail.com>
2020-04-04 23:42:53 +0530

    Initial commit

---
+++ app/src/main/java/com/root/socko/koiskeducation/MainActivity.java
@@ -0,0 +1,28 @@
+package com.root.socko.koiskeducation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout funda;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        funda = findViewById(R.id.funda);
+
+        funda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
